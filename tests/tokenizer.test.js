6ce55c5aeac706e11ab013ef558c3cc8fd519689
Mochitto <mochittodeveloper@gmail.com>
2022-10-07 01:26:13 +0200

    Tokenizer working and added tests

---
+++ tests/tokenizer.test.js
@@ -0,0 +1,47 @@
+const lexer = require("../lexer")
+const tokenizer = require("../tokenizer")
+
+function get_tokens(message) {
+    let stream = lexer(message)
+    let token_stream = tokenizer(stream)
+    let tokens = []
+    while (!token_stream.eof()) {
+        tokens.push(token_stream.next())
+    }
+    return tokens
+}
+const testing_tokens = {
+    whitespaces : {
+        input : "     \n\t\n",
+        output : []
+    },
+    string : {
+        input:'("Hello \\"world\\"!")',
+        output: [{type: "punc", value: "("}, {type: "str", value: "Hello \"world\"!"}, {type: "punc", value: ")"}]
+    },
+    recurse_example: {
+        input: "(first (list 1 (+ 2 3) 9))",
+        output: [
+            {type: "punc", value: "("},
+            {type: "kw", value: "first"},
+            {type: "punc", value: "("},
+            {type: "kw", value: "list"},
+            {type: "num", value: 1},
+            {type: "punc", value: "("},
+            {type: "op", value: "+"},
+            {type: "num", value: 2},
+            {type: "num", value: 3},
+            {type: "punc", value: ")"},
+            {type: "num", value: 9},
+            {type: "punc", value: ")"},
+            {type: "punc", value: ")"}
+        ]
+    }
+
+}
+
+for (let token_test of Object.keys(testing_tokens)) {
+    test(`Testing for ${token_test}`, () => {
+        expect(get_tokens(testing_tokens[token_test]["input"])).toEqual(testing_tokens[token_test]["output"])
+    })
+}
--- tokenizer.js
+++ tokenizer.js
@@ -1,4 +1,8 @@
 function tokenizer(stream) {
+    let current = null
+    const keywords = [
+        "and", "or", "not", "t", "NIL", "max", 
+        "min", "format", "first", "list"]
     return {
         peek: peek,
         next: next,
@@ -14,7 +18,110 @@ function tokenizer(stream) {
             skip_comment()
             return read_next()
         } 
+        else if (char == '"') return read_string()
+        else if (is_digit(char)) return read_number()
+        else if (is_id_start(char)) return read_ident()
+        else if (is_punc(char)) return {type: "punc", value: stream.next()}
+        else if (is_op_char(char)) return {type: "op", value: read_while(is_op_char)}
+        stream.croak()
     }
+
+    function read_while(predicate) {
+        let str = ""
+        while (!stream.eof() && predicate(stream.peek())) {
+            str += stream.next()
+        }
+        return str
+    }
+
+    function is_digit(char) {
+        return /\d/.test(char)
+    };
+
+    function is_id_start(char) {
+        return /[a-z_]/i.test(char)
+    }
+
+    function is_op_char(char) {
+        return /[+-/\*=<>]/.test(char)
+    }
+
+    function is_punc(char) {
+        return /[()]/.test(char)
+    }
+
+    function is_whitespace(char) {
+        return /\s/.test(char)
+    }
+
+    function is_id(char) { // You can also add extra chars here vvvvvvvvvvvv
+        return is_id_start(char) // || "other characters".indexOf(char) >= 0
+    }
+
+    function is_keyword(keyword) {
+        return keywords.includes(keyword)
+    }
+
+    function read_number(char) {
+        let has_dot = false
+        let number = read_while((char) => {
+            if (char == ".") {
+                if (has_dot) return false;
+                has_dot = true;
+                return true;
+            }
+            return is_digit(char)
+        })
+        return {type: "num", value: parseFloat(number)}
+    }
+
+    function read_ident() {
+        let id = read_while(is_id) 
+        return {type: is_keyword(id) ? "kw" : "var", value: id}
+    }
+
+    function read_escaped(special_char) {
+        let escaped = false, str = ""
+        stream.next()
+        while (!stream.eof()) {
+            let char = stream.next()
+            if (escaped) {
+                str += char
+                escaped = false
+            } else if (char == "\\") {
+                escaped = true
+            } else if (char == special_char) {
+                break
+            } else {
+                str += char
+            }
+        }
+        return str
+    }
+
+    function read_string() {
+        return {type: "str", value: read_escaped('"')}
+    }
+
+    function skip_comment() {
+        read_while((char) => char != "\n")
+        stream.next();
+    }
+
+    function peek() {
+        return current || (current = read_next())
+    }
+
+    function next() {
+        let token = current;
+        current = null;
+        return token || read_next() 
+    }
+
+    function eof() {
+        return peek() == null
+    }
+
 }
 
-export {tokenizer}
+module.exports = tokenizer
